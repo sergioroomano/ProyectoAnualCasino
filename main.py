33a96f58e87226bf6ae8bdd90cb5a0be055c1791
Mascaro101 <mascaro400@gmail.com>
2024-04-13 19:21:45 +0000

    Add bet dictionaries to roulette functionality

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def select_juego():
 def ruleta():
     return render_template('ruleta.html',resultado=None)
 
-# Funcion Archaica de lo que seria girar la ruleta CAMBIAR
+# Funcion que Controla la admision de datos desde la front-end
+# Los datos recibidos estan en el formato de [{ficha_nombre_id: [zonas_ocupadas]}]
 @app.route('/drop-zone', methods=['POST'])
 def handle_drop_zone():
     print("Chip Data Recieved")
@@ -29,16 +30,65 @@ def handle_drop_zone():
     funcionalidad_ruleta()
     return jsonify({"message": "Data received"}), 200
 
+# Función base de la funcionalidad de la ruleta
 def funcionalidad_ruleta():
+    # Inicialmente separa los datos recibidos para que puedan ser procesados posteriormente
+    numero_ganador = random.randint(0,37)
+    print("NUMERO GANADOR: ", numero_ganador)
+
+    numeros_rojos = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+    numeros_negros = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+    numeros_par =  [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
+    numeros_impar = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
+    numeros_1_12 = list(range(1,13))
+    numeros_13_24 = list(range(13,25))
+    numeros_25_37 = list(range(25,37))
+    numeros_1_18 = list(range(1,19))
+    numeros_19_37 = list(range(19,37))
+    numeros_2_1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
+    numeros_2_2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
+    numeros_2_3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+
+    posibles_apuestas = {
+    "rojo": numeros_rojos,
+    "negro": numeros_negros,
+    "par": numeros_par,
+    "impar": numeros_impar,
+    "1_12": numeros_1_12,
+    "13_24": numeros_13_24,
+    "25_37": numeros_25_37,
+    "1_18": numeros_1_18,
+    "19_37": numeros_19_37,
+    "2_1": numeros_2_1,
+    "2_2": numeros_2_2,
+    "2_3": numeros_2_3
+    }
+
+    apuestas_ganadoras_literal = []
+    apuestas_ganadoras_numerico = []
+
+    # apuestas ganadas
+    for apuesta in posibles_apuestas:
+        if numero_ganador in posibles_apuestas[apuesta]:
+            apuestas_ganadoras_literal.append(apuesta)
+            apuestas_ganadoras_numerico.append(posibles_apuestas[apuesta])
+
+    print("APUESTAS GANADORAS LITERAL: ", apuestas_ganadoras_literal)
+    print("APUESTAS GANADORAS NUMERICO: ", apuestas_ganadoras_numerico)
+
+
     for entry in session["data"]:
         for ficha in entry:
             print("FICHA: ", ficha)
             ficha_valor = int((ficha.split("-")[1]).split("_")[0])
+
             ficha_posiciones = []
-
             for zone in entry[ficha]:
-                ficha_posiciones.append(zone.split("-")[1])
-
+                zona_con_ficha = zone.split("-")[1]
+                if zona_con_ficha.isdigit():
+                    ficha_posiciones.append(zona_con_ficha)
+                else:
+                    ficha_posiciones.append(posibles_apuestas[zona_con_ficha])
 
             print("FICHA VALOR: ", ficha_valor)
             print("FICHA POSICION: ", ficha_posiciones)
